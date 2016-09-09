5e17a89a3a46c56ba75a6bd3da19a8f08e0fc94c
Scott McCormack <scott.mccormack@articulate.com>
2016-09-09 17:32:56 -0400

    First stab at a job system

---
+++ index.js
@@ -0,0 +1,38 @@
+const Consumer = require('sqs-consumer')
+const Producer = require('sqs-producer')
+
+const { compose } = require('ramda')
+const { parse, stringify } = JSON
+
+exports.create = ({ queueUrl }) => {
+  const handlers = {},
+        handleMessage = parseFirst(handleWith(handlers)),
+        consumer = Consumer.create({ handleMessage, queueUrl }),
+        producer = Producer.create({ queueUrl })
+
+  const deliver = msg => new Promise((res, rej) =>
+    producer.send([stringify(msg)], err => err ? rej(err) : res(msg)))
+
+  const handle = (type, handler) =>
+    handlers[type] = handler
+
+  const send = compose(deliver, message)
+
+  const start = consumer.start.bind(consumer)
+  const stop  = consumer.stop.bind(consumer)
+
+  return { handle, send, start, stop }
+}
+
+exports.domainify = fn => (payload, done) => {
+  const d = require('domain').create()
+  d.on('error', done)
+  d.run(fn, payload, done)
+}
+
+const handleWith = handlers => ({ type, payload }, done) =>
+  typeof handlers[type] === 'function' && handlers[type](payload, done)
+
+const message = (type, payload) => ({ type, payload })
+
+const parseFirst = fn => (msg, done) => fn(parse(msg.Body), done)
