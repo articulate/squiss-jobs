07b677fd79584587dbf870f0e1db249a2d109703
Kevin Stewart <kstewart@articulate.com>
2017-02-10 11:46:18 -0500

    wip adding promises.  Ignore the commented timeout stuff

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Consumer = require('sqs-consumer')
 const idgen    = require('idgen')
 const Producer = require('sqs-producer')
+const Promise  = require('bluebird')
+const { nAry } = require('ramda')
 
 const { always, compose, merge } = require('ramda')
 const { parse, stringify } = JSON
 
+Promise.config({ cancellation: true })
+
 exports.create = options => {
+  console.log('OPTIONS: ', options)
   const handlers = {},
-        handleMessage = parseFirst(handleWith(handlers)),
+        handleMessage = parseFirst(handleWith(handlers, options.visibilityTimeout)),
         consumer = Consumer.create(merge(options, { handleMessage })),
         producer = Producer.create(options),
         queue    = {}
@@ -34,15 +39,43 @@ exports.create = options => {
   return queue
 }
 
-exports.domainify = fn => (payload, done) => {
-  const d = require('domain').create()
-  d.on('error', done)
-  d.run(fn, payload, done)
-}
+//exports.domainify = fn => (payload, done) => {
+  //const d = require('domain').create()
+  //d.on('error', done)
+  //d.run(fn, payload, done)
+//}
 
 const action = (type, payload) => ({ type, payload })
 
-const handleWith = handlers => ({ type, payload }, done) =>
-  typeof handlers[type] === 'function' ? handlers[type](payload, done) : done(new Error(`No Handler registered for (${type})`))
+//const wrapWithTimeout = (fn, timeout) => payload => fn(payload).timeout(timeout, `Squiss-Jobs: Consumer job timmed out after ${timeout}`)
+
+//const handleWith = (handlers, timeout=30000) => ({ type, payload }, done) => {
+  //if (typeof handlers[type] === 'function') {
+    ////return Promise.resolve(handlers[type](payload))
+    //return Promise.resolve(payload)
+      //.then(wrapWithTimeout(handlers[type], timeout))
+      ////.then((v) => {
+        ////handlers[type](v)
+          ////.timeout(timeout, `Squiss-Jobs: Consumer job timmed out after ${timeout}`)
+      ////})
+      //.then((v)=>{console.log('END: ', v); return v;})
+      //.then(nAry(0, done))
+      ////.catch(Promise.TimeoutError, (err) => {console.log('cancelling maybe'); throw err})
+      //.catch(done)
+  //} else {
+    //return done(new Error(`No Handler registered for (${type})`))
+  //}
+//}
+
+const handleWith = (handlers) => ({ type, payload }, done) => {
+  if (typeof handlers[type] === 'function') {
+    return Promise.resolve(payload)
+      .then(handlers[type])
+      .then(nAry(0, done))
+      .catch(done)
+  } else {
+    return done(new Error(`No Handler registered for (${type})`))
+  }
+}
 
 const parseFirst = fn => (msg, done) => fn(parse(msg.Body), done)
